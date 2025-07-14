edbc6f8815b9a761a4b3e17a91bc27d72ecbff09
j-anubhav <anubhavjha.dev@gmail.com>
2025-07-14 23:15:45 +0530

    added a analytics view to see hoe many click you have done in the link early you can see it thorough database now you can see it on http://localhost:8001/url/analytics/:shortId

--- 03- URL Shortener/controllers/url.js
+++ 03- URL Shortener/controllers/url.js
@@ -18,6 +18,15 @@ async function handleGenerateNewShortURL(req, res) {
 
 }
 
+
+async function handleGetAnalytics(req,res) {
+
+    const shortId =req.params.shortId;
+    const result = await URL.findOne({shortId});
+
+    return res.json({totalclick:result.visitHistory.length, analytics :result.visitHistory})
+}
 module.exports = {
     handleGenerateNewShortURL,
+    handleGetAnalytics,
 }
