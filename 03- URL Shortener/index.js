7e94aaa635216285ad36b3bbccb88d7168d7025a
j-anubhav <anubhavjha.dev@gmail.com>
2025-07-15 22:01:15 +0530

    done SSR

--- 03- URL Shortener/index.js
+++ 03- URL Shortener/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const urlRoute= require('./routes/url')
-const {connectToMonogoDB} = require('./connect')
-const URL = require('./models/url')
+const path =require('path');
+const urlRoute= require('./routes/url');
+const {connectToMonogoDB} = require('./connect');
+const URL = require('./models/url');
 
 const app = express();
 const PORT =8001;
@@ -9,11 +10,16 @@ const PORT =8001;
 connectToMonogoDB('mongodb://localhost:27017/short-url')
 .then(()=>{console.log('MOnogDB connected')})
 
-app.use(express.json())
+app.set("view engine" , "ejs");
+app.set('views',path.resolve('./views') )
+
+app.use(express.json());
+
+
 
 app.use('/url', urlRoute);
 
-app.get('/:shortId' , async(req,res) =>{
+app.get('/url/:shortId' , async(req,res) =>{
     const shortId =req.params.shortId;
     const entry =await URL.findOneAndUpdate({
         shortId
